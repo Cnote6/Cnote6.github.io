86edb9c8ab3208e68c1c2a1894503af2e5958c8c
Cnote6 <95633278+Cnote6@users.noreply.github.com>
2022-04-13 22:42:45 -0400

    Add files via upload

--- cartbutton.js
+++ cartbutton.js
@@ -1,12 +1,12 @@
 	$(document).ready(function() {
 			$("#cartBtn").click(function() {
-			  let quantity =  document.getElementById('quantity');
-			let prices = document.getElementById('prices1');
-			var text = $('prices1').text();
-			let total = quantity * number;
-			var number = parseInt(text, 10);
-			alert(number);
-			let name = document.getElementById("name");
-			 alert( number + " added to cart");
-			});
+			  let quantity =  document.getElementById("quantity").value;
+			let prices = document.getElementById("prices1");
+			let priceNum = parseFloat(3.55); 
+			let total =  quantity * priceNum;
+		
+			let product = $("name").val();
+			
+			 alert(total + " added to cart");
+	});
 			});
