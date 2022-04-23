55268cbfb41d8650dd44902e8be07ee66c118ea2
Cnote6 <95633278+Cnote6@users.noreply.github.com>
2022-04-23 15:16:24 -0400

    Add files via upload

---
+++ FAQS.js
@@ -0,0 +1,29 @@
+
+   $(document).ready(function() {
+  $('#moreBtn1').click(function() {
+  $('#p1').toggle("slow");
+  });
+  });
+  
+  $(document).ready(function() {
+  $('#moreBtn2').click(function() {
+  $('#p2').toggle("slow");
+  });
+  });
+  $(document).ready(function() {
+  $('#moreBtn3').click(function() {
+  $('#p3').toggle("slow");
+  });
+  });
+  $(document).ready(function() {
+  $('#moreBtn4').click(function() {
+  $('#p4').toggle("slow");
+  });
+  });
+  $(document).ready(function() {
+  $('#moreBtn5').click(function() {
+  $('#p5').toggle("slow");
+  });
+  });
+  
+  
--- cartbutton.js
+++ cartbutton.js
@@ -1,11 +1,95 @@
+
 	$(document).ready(function() {
 			$("#cartBtn").click(function() {
 			  let quantity =  document.getElementById("quantity").value;
 			let prices = document.getElementById("prices1");
 			let priceNum = parseFloat(3.55); 
 			let total =  quantity * priceNum;
-		
-			let product = $("name").val();
-			 alert(` ${product}: $ ${total} added to cart`);
+	       let product =  document.getElementById("name").value;
+			 alert(` Button Mushrooms: $ ${total} added to cart`);
 	});
 			});
+			
+						$(document).ready(function() {
+			$("#cartBtn2").click(function() {
+			  let quantity =  document.getElementById("quantity1").value;
+			let prices = document.getElementById("prices2");
+			let priceNum = 6;
+			let total =  quantity * priceNum;
+	       
+			let product = document.getElementById("name1").value;
+			 alert(` Cremini mushrooms:  $ ${total} added to cart`);
+	});
+			});
+						$(document).ready(function() {
+			$("#cartBtn3").click(function() {
+			  let quantity =  document.getElementById("quantity2").value;
+			let prices = document.getElementById("prices3");
+			let priceNum = 1.69;
+			let total =  quantity * priceNum;
+	       
+			let product = document.getElementById("name1").value;
+			 alert(` Portobello mushrooms:  $ ${total} added to cart`);
+	});
+			});
+			
+						$(document).ready(function() {
+			$("#cartBtn4").click(function() {
+			  let quantity =  document.getElementById("quantity3").value;
+			let prices = document.getElementById("prices4");
+			let priceNum = 6;
+			let total =  quantity * priceNum;
+	       
+			let product = document.getElementById("name1").value;
+			 alert(` Oyster mushrooms:  $ ${total} added to cart`);
+	});
+			});
+			
+						$(document).ready(function() {
+			$("#cartBtn5").click(function() {
+			  let quantity =  document.getElementById("quantity4").value;
+			let prices = document.getElementById("prices5");
+			let priceNum = 48;
+			let total =  quantity * priceNum;
+	       
+			let product = document.getElementById("name1").value;
+			 alert(` Chanterelle mushrooms:  $ ${total} added to cart`);
+	});
+			});
+			
+						$(document).ready(function() {
+			$("#cartBtn6").click(function() {
+			  let quantity =  document.getElementById("quantity5").value;
+			let prices = document.getElementById("prices6");
+			let priceNum = 48;
+			let total =  quantity * priceNum;
+	       
+			let product = document.getElementById("name1").value;
+			 alert(` Morel mushrooms:  $ ${total} added to cart`);
+	});
+			});
+			
+						$(document).ready(function() {
+			$("#cartBtn7").click(function() {
+			  let quantity =  document.getElementById("quantity6").value;
+			let prices = document.getElementById("prices7");
+			let priceNum = 12;
+			let total =  quantity * priceNum;
+	       
+			let product = document.getElementById("name1").value;
+			 alert(` Shiitake mushrooms:  $ ${total} added to cart`);
+	});
+			});
+			
+						$(document).ready(function() {
+			$("#cartBtn8").click(function() {
+			  let quantity =  document.getElementById("quantity7").value;
+			let prices = document.getElementById("prices8");
+			let priceNum = 75;
+			let total =  quantity * priceNum;
+	       
+			let product = document.getElementById("name1").value;
+			 alert(` Reishi mushrooms:  $ ${total} added to cart`);
+	});
+			});
+			
